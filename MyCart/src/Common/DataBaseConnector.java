b3c18de48c3aaa2c66209dec4fda3ca03f7bd27f
Mayukhsuri <mayukhsuri03@gmail.com>
2016-10-03 19:18:15 -0500

    updated the connection

--- MyCart/src/Common/DataBaseConnector.java
+++ MyCart/src/Common/DataBaseConnector.java
@@ -1,11 +1,14 @@
 package Common;
 
+import Common.WebFunctions;
 //import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import tables.Order;
 import tables.Product;
 
 public class DataBaseConnector {
@@ -17,44 +20,89 @@ public class DataBaseConnector {
               
 
 	 */
-	private String address = "Mayukh";
+	private String address = "jdbc:mysql://localhost:3307/mycart";
 
-	private String MYSQL_username = "Mayukh";
-	private String MYSQL_password = "mayukh";
+	private String MYSQL_username = "root";
+	private String MYSQL_password = "Mayukh123";
 	
 	private Statement statement = null;
-	public Product search(int productID) {
+	public Order orderaccept(int orderquantity)
+	{
+		Order acceptobj = new Order();
+		try{
+			Class.forName("").newInstance();
+			Connection connection = DriverManager.getConnection(MYSQL_password, MYSQL_username,address);
+			statement = connection.createStatement();
+			ResultSet resultset2 = null;
+			String sql2 = "Select from order WHERE Orderquantity =2 ;";
+			resultset2 = statement.executeQuery(sql2);
+			
+			if(orderquantity<1)
+			{
+				System.out.println("Order cannot be placed.");
+				resultset2.next();
+			}
+			else
+			{
+				System.out.println("Your order is placed.");
+
+			}
+		}
+			catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException ex) {
+				
+			    // handle any errors
+			    //System.out.println("SQLException: " + ex.getMessage());
+			    //System.out.println("SQLState: " + ((SQLException) ex).getSQLState());
+			   // System.out.println("VendorError: " + ((SQLException) ex).getErrorCode());
+			}
+		return acceptobj;
+		}
 	
-		Product product = new Product();
-		
+	public Product search(int product_ID) {
 	
-	
+		Product product = new Product(1001); 
 	try {
 		Class.forName("").newInstance();
 		
 	
 		Connection connection = DriverManager.getConnection(MYSQL_password, MYSQL_username,address);
 		statement = connection.createStatement();
-		ResultSet result = null;
-		String query ="Select * from product";
-		result = statement.executeQuery(query);
-		if(result!=null)
+		ResultSet resultset1 = null;
+		String sql ="Select * from product WHERE product_ID=1001";
+		resultset1 = statement.executeQuery(sql);
+		
+		if(resultset1!=null)
 		{
 			System.out.println("processing ..");
-			result.next();
-			product.setProductID(result.getInt("ProductID"));
-			product.setProductdescription(result.getString("productDescription"));
-			product.setUnitprice(result.getInt("unitprice"));
+			resultset1.next();
+			product.setProductID(resultset1.getInt("ProductID"));
+			product.setProductdescription(resultset1.getString("productDescription"));
+			product.setUnitprice(resultset1.getInt("unitprice"));
 		}
+
+		
+	/*	if(resultset1!= null)
+		{
+			System.out.println("processing ..");
+			product.setproduct_ID(resultset1.getInt("product_ID"));
+			product.setProduct_description(resultset1.getString("product_description"));
+			product.setUnit_price(resultset1.getInt("unit_price"));
+		}
+		*/
+		;
+		
+		
 	} catch (SQLException | InstantiationException | IllegalAccessException | ClassNotFoundException ex) {
 		
 	    // handle any errors
-	    System.out.println("SQLException: " + ex.getMessage());
-	    System.out.println("SQLState: " + ((SQLException) ex).getSQLState());
-	    System.out.println("VendorError: " + ((SQLException) ex).getErrorCode());
+	    //System.out.println("SQLException: " + ex.getMessage());
+	    //System.out.println("SQLState: " + ((SQLException) ex).getSQLState());
+	    //System.out.println("VendorError: " + ((SQLException) ex).getErrorCode());
 	}
 	return product;
 	}
+	
+	
 }
 
 
